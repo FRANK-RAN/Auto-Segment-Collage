2200497b979c1430edfd89045a13cf5293f13255
Evan810828 <yw185@rice.edu>
2024-03-28 17:22:51 -0500

    init & clip test complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Auto-Segment-Collage
+
+download animal90 dataset
+```bash
+
+```
---
+++ demo/animal90_dataset.py
@@ -0,0 +1,20 @@
+import gdown
+import zipfile
+import os
+
+url = 'https://drive.google.com/uc?id=1XykJthWV9bWpzwccaBOBtc0aNJ6YI-94'
+output_path = 'data/animal90.zip'
+
+gdown.download(url, output_path, quiet=False)
+
+# Unzip the files
+zip_file_path = 'data/animal90.zip'
+destination_directory = 'data/'
+
+os.makedirs(destination_directory, exist_ok=True)
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    for file in zip_ref.namelist():
+        if file.startswith('animals/animals/'):
+            new_path = os.path.join(destination_directory, file.replace('animals/animals/', 'animals/'))
+            zip_ref.extract(file, new_path)
