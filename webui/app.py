b848d7612958f81281486ee157fc0f7b9140426c
Evan810828 <yw185@rice.edu>
2024-04-18 22:38:41 -0500

    webui

---
+++ webui/app.py
@@ -0,0 +1,79 @@
+import gradio as gr
+import pdb
+
+def update(input_cate):
+    return gr.Dropdown(
+            ["tigers", "dogs", "cats", "Feifeis"], 
+            label="Available Genres", 
+            interactive=True,
+            info="Segementation and classification completed. Please choose the genre you would like to view."
+        )
+
+def get_select_value(selected_gallery, evt: gr.SelectData):
+    if selected_gallery:
+        selected_gallery.append(evt.value['image']['path'])
+    else:
+        selected_gallery = gr.Gallery(
+            [evt.value['image']['path']], columns=[5], object_fit="contain", height=200, show_label=False, elem_id="gallery", label="Selected Images", interactive=False
+        )
+    return selected_gallery
+
+def collage():
+    return "http://www.marketingtool.online/en/face-generator/img/faces/avatar-11319be65db395d0e8e6855d18ddcef0.jpg"
+
+with gr.Blocks() as demo:    
+    gr.Markdown('''
+        # Welcome to Auto-Collage
+
+        Welcome to **Auto-Collage**, 
+        your go-to app for generating custom collages! 
+        Whether you're an educator seeking engaging visual aids or an artist looking for inspiration, 
+        our app provides a user-friendly platform to create unique collages of segmented images. 
+        Powered by advanced AI models SAM and CLIP, we ensure high-quality, precision-segmented visuals. 
+        Dive into our interactive interface below and start creating your personalized collage today!
+                ''')
+    with gr.Row():
+        with gr.Group():
+            inp = gr.Textbox(
+                placeholder="Enter the path to your image dataset here, and click the `Process` button",
+                label = "Dataset Path",
+            )
+            process_btn = gr.Button("Process")
+        options = gr.Dropdown(
+            [], 
+            label="Available Genres", 
+            interactive=True,
+            info="Wait segementation and classification to start."
+        )
+    with gr.Row():
+        with gr.Column():
+            gallery = gr.Gallery(
+                [
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1151ce9f4b2043de0d2e3b7826127998.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-116b5e92936b766b7fdfc242649337f7.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1163530ca19b5cebe1b002b8ec67b6fc.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1116395d6e6a6581eef8b8038f4c8e55.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-11319be65db395d0e8e6855d18ddcef0.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1151ce9f4b2043de0d2e3b7826127998.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-116b5e92936b766b7fdfc242649337f7.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1163530ca19b5cebe1b002b8ec67b6fc.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-1116395d6e6a6581eef8b8038f4c8e55.jpg",
+                "http://www.marketingtool.online/en/face-generator/img/faces/avatar-11319be65db395d0e8e6855d18ddcef0.jpg",
+                ], columns=[5], object_fit="contain", show_label=False, elem_classes="gallery", height=300, label="Available Images", interactive=False
+            )
+            gr.Markdown('''
+                Select the images you would like to include in your collage by clicking on them.
+            ''')
+            selected_gallery = gr.Gallery(
+                [], columns=[5], object_fit="contain", show_label=False, elem_classes="gallery", height=200, label="Selected Images", interactive=False
+            )
+            collage_btn = gr.Button("Generate Collage")
+        output = gr.Image(label="Collage")
+    
+    # Events
+    gallery.select(get_select_value, selected_gallery, selected_gallery)
+    process_btn.click(fn=update, inputs=inp, outputs=[options])
+    collage_btn.click(collage, None, output)
+
+if __name__ == "__main__":
+    demo.launch(share=True)
